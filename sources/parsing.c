7a5f32f7878f822272c1269d6cdc975ad440bee2
resher69 <resher.ag@gmail.com>
2021-09-02 20:35:55 -0400

    parsing done

--- sources/parsing.c
+++ sources/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: kali <kali@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 02:45:30 by agardet           #+#    #+#             */
-/*   Updated: 2021/08/28 06:29:18 by agardet          ###   ########lyon.fr   */
+/*   Updated: 2021/09/02 20:04:39 by kali             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ void	ft_get_nbsqty(char **list, t_data *data)
 	}
 }
 
+//count how many numbers in the list
 void	ft_check_a_stack_error(int argc, char **argv, int *a_stack, char **list)
 {
 	int	i;
@@ -55,6 +56,7 @@ void	ft_check_a_stack_error(int argc, char **argv, int *a_stack, char **list)
 
 }
 
+//put the input in char **list and verify if input is correct
 int	*ft_argv_to_tab(int argc, char **argv, t_data *data)
 {
 	int		*a_stack;
@@ -77,14 +79,18 @@ int	*ft_argv_to_tab(int argc, char **argv, t_data *data)
 	return (a_stack);
 }
 
+//creates the A stack then sort the numbers inside it
 int	*ft_parsing(int argc, char **argv, t_data *data)
 {
 	int	*a_stack;
 
 	if (argc < 2)
 		ft_error();
-	a_stack = argv_to_tab(argc, argv, data);
+	a_stack = ft_argv_to_tab(argc, argv, data);
 	if (!a_stack)
+		ft_error();
+	a_stack = ft_sort_list(a_stack, data);
+		if (!a_stack)
 		ft_error();
 	return (a_stack);
 }
--- sources/utils.c
+++ sources/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: kali <kali@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 02:53:19 by agardet           #+#    #+#             */
-/*   Updated: 2021/08/28 06:29:16 by agardet          ###   ########lyon.fr   */
+/*   Updated: 2021/09/02 20:04:37 by kali             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,4 @@ void	ft_error(void)
 {
 	write(1, "Error\n", 6);
 	exit(EXIT_FAILURE);
-}
-
-long	ft_modified_atoi(char **argv, int *a_stack, int i, int count_qty)
-{
-	long	nb;
-	int		roam;
-	int		j;
-
-	nb = 0;
-	roam = 0;
-	j = 0;
-	if (argv[i][roam] == '-')
-		roam++;
-	while (argv[i][roam] && argv[i][roam] >= '0' && argv[i][roam] <= '9')
-	{
-		nb = nb * 10 + argv[i][roam] - '0';
-		roam++;
-	}
-	if (argv[i][0] == '-')
-		nb *= -1;
-	if (nb < INT_MIN || nb > INT_MAX)
-		ft_error();
-	while (++j < count_qty)
-	{
-		if (a_stack[i] == nb)
-			ft_error();
-	}
-	return (nb);
 }
